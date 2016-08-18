6553295df14e2b2096d3718ca327bd8b93125e3b
Jaymin <jayminpanchal10@gmail.com>
2016-08-18 23:38:09 +0530

    Multiple compoenent header and gome

---
+++ 3.MultipleComponent/src/app/component/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {render} from "react-dom";
+
+export class Home extends React.Component {
+    render() {
+        return (
+            <section className="content">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h2>Home Page</h2>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
---
+++ 3.MultipleComponent/src/app/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {render} from "react-dom";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1>Hello!!</h1>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+render(<App/>, window.document.getElementById("app"));
