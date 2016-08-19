aa09c9d241955b2ccc0574a67ed38838a93c479e
Jaymin <jayminpanchal10@gmail.com>
2016-08-20 01:02:07 +0530

    Props with object pasing and body content

--- 5.Props/src/app/ccomponent/Home.js
+++ 5.Props/src/app/ccomponent/Home.js
@@ -6,14 +6,24 @@ var Home = React.createClass({
             <section className="content">
                 <div className="row">
                     <div className="col-md-12">
-                        <h2>{this.props.homeTitle} </h2>
+                        <h2>{this.props.title} </h2>
                         <hr/>
-                        <p>{this.props.homeDescription} </p>
+                        <p>{this.props.description} </p>
+                        <br/>
+                        <h3>User Details:</h3>
+                        <hr/>
+                        <p>User Name:{this.props.user.name}, Email: {this.props.user.name}, Mobile
+                            No: {this.props.user.mobile} </p>
+                        <br/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12">
+                        {this.props.children}
                     </div>
                 </div>
             </section>
         );
     }
 });
-
 module.exports = Home;
--- 5.Props/src/app/index.js
+++ 5.Props/src/app/index.js
@@ -4,19 +4,23 @@ var Header = require("./ccomponent/Header");
 var Home = require("./ccomponent/Home");
 
 var App = React.createClass({
-    render: function () {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <Header/>
-                        {/*It is a basic Data Passing*/}
-                        <Home homeTitle={"Home Page"} homeDescription={"It is a home page."}/>
+        render: function () {
+            let user = {"name": "ABC", "mobile": "1234567890", "email": "abc@gmail.com"};
+            return (
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <Header/>
+                            {/*It is a basic Data Passing*/}
+                            <Home title={"Home Page"} description={"It is a home page."} user={user}>
+                                <p>This is the data passed in a body of Home Tag</p>
+                            </Home>
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
-    }
-});
+            );
+        }
+    })
+    ;
 
 ReactDOM.render(<App/>, document.getElementById("app"));
