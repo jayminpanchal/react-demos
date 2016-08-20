9827222c4c37be4af7476b2a93697649e09d1b28
Jaymin <jayminpanchal10@gmail.com>
2016-08-21 00:42:09 +0530

    basic updated

--- 5.Props/src/app/index.js
+++ 5.Props/src/app/index.js
@@ -4,28 +4,27 @@ var Header = require("./ccomponent/Header");
 var Home = require("./ccomponent/Home");
 
 var App = React.createClass({
-        render: function () {
-            let user = {
-                name: "ABC",
-                mobile: "1234567890",
-                email: "abc@gmail.com",
-                hobbies: ["Sports", "Reading", "Coding"]
-            };
-            return (
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12">
-                            <Header/>
-                            {/*It is a basic Data Passing*/}
-                            <Home title={"Home Page"} description={"It is a home page."} user={user}>
-                                <p>This is the data passed in a body of Home Tag</p>
-                            </Home>
-                        </div>
+    render: function () {
+        let user = {
+            name: "ABC",
+            mobile: "1234567890",
+            email: "abc@gmail.com",
+            hobbies: ["Sports", "Reading", "Coding"]
+        };
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <Header/>
+                        {/*It is a basic Data Passing*/}
+                        <Home title={"Home Page"} description={"It is a home page."} user={user}>
+                            <p>This is the data passed in a body of Home Tag</p>
+                        </Home>
                     </div>
                 </div>
-            );
-        }
-    })
-    ;
+            </div>
+        );
+    }
+});
 
 ReactDOM.render(<App/>, document.getElementById("app"));
