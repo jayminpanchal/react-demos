3f717d0324858f5b42f20418721151d8d6500ba5
Jaymin <jayminpanchal10@gmail.com>
2016-08-20 16:43:44 +0530

    Simple event binding to button click

--- 5.Props/src/app/ccomponent/Home.js
+++ 5.Props/src/app/ccomponent/Home.js
@@ -1,33 +1,38 @@
 var React = require("react");
 
 var Home = React.createClass({
-    render: function () {
-        return (
-            <section className="content">
-                <div className="row">
-                    <div className="col-md-12">
-                        <h2>{this.props.title} </h2>
-                        <hr/>
-                        <p>{this.props.description} </p>
-                        <br/>
-                        <h3>User Details:</h3>
-                        <hr/>
-                        <p>User Name:{this.props.user.name}, Email: {this.props.user.name}, Mobile
-                            No: {this.props.user.mobile} </p>
-                        <br/>
-                        <h4>Hobbies</h4>
-                        <ul>
-                            {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
-                        </ul>
+        clickMe: function () {
+            alert("Button Clicked!!");
+        },
+        render: function () {
+            return (
+                <section className="content">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h2>{this.props.title} </h2>
+                            <hr/>
+                            <p>{this.props.description} </p>
+                            <br/>
+                            <h3>User Details:</h3>
+                            <hr/>
+                            <p>User Name:{this.props.user.name}, Email: {this.props.user.name}, Mobile
+                                No: {this.props.user.mobile} </p>
+                            <br/>
+                            <h4>Hobbies</h4>
+                            <ul>
+                                {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
+                            </ul>
+                            <button className="btn btn-info" onClick={this.clickMe}>Click Me!</button>
+                        </div>
                     </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-12">
-                        {this.props.children}
+                    <div className="row">
+                        <div className="col-md-12">
+                            {this.props.children}
+                        </div>
                     </div>
-                </div>
-            </section>
-        );
-    }
-});
+                </section>
+            );
+        }
+    })
+    ;
 module.exports = Home;
