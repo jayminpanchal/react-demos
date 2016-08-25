4926d45682ca45d9469ca6ae2f35f163404d824c
Jaymin <jayminpanchal10@gmail.com>
2016-08-26 01:10:44 +0530

    Added eventto unmount the home component

--- 8.ComponentLifeCycle/src/app/index.js
+++ 8.ComponentLifeCycle/src/app/index.js
@@ -4,14 +4,30 @@ var Header = require("./component/Header");
 var Home = require("./component/Home");
 
 var App = React.createClass({
+    getInitialState: function () {
+        return {
+            homeMounted: true
+        };
+    },
+    onChangeHomeMounted: function () {
+        this.setState({
+            homeLink: false
+        });
+    },
     render: function () {
+        let homeComponent = "";
+        if (this.state.homeMounted) {
+            homeComponent = ' <Home title={"Home Page"} description={"It is a home page."}> </Home>';
+        }
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-md-12">
                         <Header/>
-                        <Home title={"Home Page"} description={"It is a home page."}>
-                        </Home>
+                        {homeComponent}
+                        <button className="btn btn-danger" onClick={this.onChangeHomeMounted.bind(this)}>
+                            (Un)Mount Home Component
+                        </button>
                     </div>
                 </div>
             </div>
